35e298310267360ee784ce0b37cebf40f5521c6c
JesicaJunco <93754061+JesicaJunco@users.noreply.github.com>
2022-08-26 16:09:18 -0300

    Creacion de estructura

---
+++ ProyectoIntegrador_JuncoJesica/BackEnd/src/main/java/com/portfolio/Jesica/JesicaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Jesica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JesicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JesicaApplication.class, args);
+	}
+
+}
