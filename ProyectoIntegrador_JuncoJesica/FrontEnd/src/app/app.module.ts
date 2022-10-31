042b3cdde18e76058a06d4655fbbb3169347e127
JesicaJunco <93754061+JesicaJunco@users.noreply.github.com>
2022-10-31 19:03:57 -0300

    Login con JWT

--- ProyectoIntegrador_JuncoJesica/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador_JuncoJesica/FrontEnd/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { interceptorProvider } from './service/interceptor-service';
 
 
 @NgModule({
@@ -39,7 +40,9 @@ import { LoginComponent } from './components/login/login.component';
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
